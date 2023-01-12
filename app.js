3ee85fbda7fc0fd9a396cc778826061c9ec2c67a
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-12 12:30:09 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const drinks = document.getElementById("drinks"); //Get stuff from html file
+const price = document.getElementById("price");
+const add = document.getElementById("add");
+const cart = document.getElementById("cart");
+const quantity = document.getElementById("quantity");
+const payButton = document.getElementById("pay");
+const total = document.getElementById("total");
+
+let drinksList = [];
+let cartList = [];
+let totalCount = 0.0;
+
