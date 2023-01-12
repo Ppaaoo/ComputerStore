14d6b5ececea52b2c8e88d5171f173a1fddf3990
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-12 15:14:20 +0100

    Banking application with basic functionality added

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const drinks = document.getElementById("drinks"); //Get stuff from html file
-const price = document.getElementById("price");
-const add = document.getElementById("add");
-const cart = document.getElementById("cart");
-const quantity = document.getElementById("quantity");
-const payButton = document.getElementById("pay");
-const total = document.getElementById("total");
+const loanButton = document.getElementById("getLoan");
+const workButton = document.getElementById("work");
+const repayLoanButton = document.getElementById("repayLoan");
+//repayLoanButton.style.visibility = 'hidden';
 
-let drinksList = [];
-let cartList = [];
-let totalCount = 0.0;
+const balanceLabel = document.getElementById("balanceLabel");
 
+let balance = 25
+let formatted = formatBalance(balance);
+document.getElementById("balanceLabel").innerHTML = formatted;
+
+function formatBalance(amount) {
+    return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(amount);
+}
+
+loanButton.addEventListener('click', () => {
+    let loan = window.prompt("Add amount to loan");
+    if (isNaN(parseInt(loan))) {
+        alert("Please enter a numeric value");
+    }
+    if (parseInt(loan) > balance * 2) {
+        alert("Loan not approved");
+    }
+    else {
+        balance = balance + parseInt(loan);
+        let formattedBalance = formatBalance(balance);
+        document.getElementById("balanceLabel").innerHTML = formattedBalance;
+    }
+})
