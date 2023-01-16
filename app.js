3e9ce07b642bb13f7f2369c549a6d17d4f475198
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-16 14:29:28 +0100

    All functionality works!

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //BankElements
 const loanButton = document.getElementById("getLoanButton");
 const repayLoanButton = document.getElementById("repayLoanButton");
+repayLoanButton.style.display = "none";
 const balanceLabel = document.getElementById("balanceLabel");
+const loanAmountEl = document.getElementById("loanAmount");
 
 //WorkElements
 const workButton = document.getElementById("workButton");
@@ -14,60 +16,126 @@ const laptopPrice = document.getElementById("price");
 const featuresEL = document.getElementById("features");
 const stockEl = document.getElementById("stock");
 const imageEl = document.getElementById("laptopImage");
+const payButton = document.getElementById("payButton");
 
 //Bank
 let balance = 25
+let hasLoan = false;
+let loanSum = 0;
+
 //Default balance to screen
-let formatted = formatBalance(balance);
-balanceLabel.innerText = formatted;
+let formattedDefaultBalance = formatBalance(balance);
+balanceLabel.innerText = formattedDefaultBalance;
+let formattedDefaultLoan = formatBalance(loanSum);
+loanAmountEl.innerText = formattedDefaultLoan;
 
 function formatBalance(amount) {
     return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'SEK'}).format(amount);
 }
 
+//Take loan functionality
 loanButton.addEventListener('click', () => {
     let loan = window.prompt("Add amount to loan");
     if (isNaN(parseInt(loan))) {
         alert("Please enter a numeric value");
-    }
-    if (parseInt(loan) > balance * 2) {
+    } else if (parseInt(loan) > balance * 2) {
         alert("Loan not approved");
-    }
-    else {
-        balance = balance + parseInt(loan);
+    } else if (hasLoan == true) {
+        alert("You already have a loan! Please pay it back before taking another loan.");
+    } else {
+        balance += parseInt(loan);
+        loanSum += loan;
         let formattedBalance = formatBalance(balance);
         balanceLabel.innerText = formattedBalance;
+        let formattedLoan = formatBalance(loanSum);
+        loanAmountEl.innerText = formattedLoan;
+        repayLoanButton.style.display = "block";
+        hasLoan = true;
     }
 })
 
 //Work
-let workPay = 0;
 let workTotal = 0;
+
 //Default pay to screen
-let formattedPay = formatBalance(0);
-payEl.innerText = formattedPay;
+let formattedDefaultPay = formatBalance(0);
+payEl.innerText = formattedDefaultPay;
 
 const getWorkMoney = () => {
-    workTotal = workPay += 100;
+    workTotal += 100;
     let formattedWorkTotal = formatBalance(workTotal);
     payEl.innerText = formattedWorkTotal;
 }
 
 const bankMoney = () => {
-    balance += workTotal;
+    if (hasLoan = true) {
+        loanSum -= (workTotal * 0.1);
+        if (loanSum <= 0) {
+            balance += Math.abs(loanSum);
+            loanSum = 0;
+            repayLoanButton.style.display = "none";
+            hasLoan = false;
+        }
+        workTotal -= (workTotal * 0.1)
+        balance += workTotal;
+        workTotal = 0;
+    }
+    if(hasLoan = false) {
+        balance += workTotal;
+        workTotal = 0;
+    }
+
     let formattedBalance = formatBalance(balance);
+    let formattedPay = formatBalance(workTotal);
+    let formattedLoan = formatBalance(loanSum);
     balanceLabel.innerText = formattedBalance;
+    payEl.innerText = formattedPay;
+    loanAmountEl.innerText = formattedLoan;
+}
+
+const repayLoan = () => {
+    if(workTotal >= loanSum) {
+        workTotal -= loanSum;
+        loanSum = 0;
+        balance += workTotal;
+        workTotal = 0;
+        repayLoanButton.style.display = "none";
+        hasLoan = false;
+    } else if (loanSum >= workTotal) {
+        loanSum -= workTotal;
+        workTotal = 0;
+    } else if (workTotal === loanSum) {
+        loanSum = 0;
+        workTotal = 0;
+        repayLoanButton.style.display = "none";
+        hasLoan = false;
+    }
+
+    let formattedBalance = formatBalance(balance);
+    let formattedPay = formatBalance(workTotal);
+    let formattedLoan = formatBalance(loanSum);
+    balanceLabel.innerText = formattedBalance;
+    payEl.innerText = formattedPay;
+    loanAmountEl.innerText = formattedLoan;
 }
 
 workButton.addEventListener('click', getWorkMoney);
 bankButton.addEventListener('click', bankMoney);
+repayLoanButton.addEventListener('click', repayLoan);
 
 //Laptops
 let laptopsArr = [];
-let featuresArr = [];
-let imageURL = "https://hickory-quilled-actress.glitch.me/assets/images/";
+let imageURL = "https://hickory-quilled-actress.glitch.me/";
+let currentLaptopPrice = 200;
+let currentLaptopId = 0;
+let stockArr = [];
+
+//Default stock
+stockArr[0] = 1;
+stockEl.innerText = stockArr[0];
 
 //Adding default features to screen. Probably not the best way of doing this, look into this later
+let featuresArr = [];
 featuresArr[0] = `Has a screen`;
 featuresArr[1] = `Keyboard works, mostly`;
 featuresArr[2] = `32MB Ram (Not upgradeable)`;
@@ -90,11 +158,13 @@ fetch("https://hickory-quilled-actress.glitch.me/computers")
 const addLaptopsToList = laptopsArr => {
     laptopsArr.forEach(x => addLaptopToList(x));
     laptopPrice.innerText = formatBalance(laptopsArr[0].price);
+
 }
 
 const addLaptopToList = laptop => {
     const laptopElement = document.createElement("option");
     laptopElement.value = laptop.id;
+    stockArr[laptop.id] = laptop.stock;
     laptopElement.appendChild(document.createTextNode(laptop.title));
     laptopEl.appendChild(laptopElement);
 }
@@ -103,13 +173,15 @@ const handleMenuChange = e => {
     removeChildElements();
     const selectedLaptop = laptopsArr[e.target.selectedIndex];
     laptopPrice.innerText = formatBalance(selectedLaptop.price);
+    currentLaptopPrice = selectedLaptop.price;
     for(i = 0; i< selectedLaptop.specs.length; i++) {
         const featureItem = document.createElement("li");
         featureItem.innerText = `${selectedLaptop.specs[i]}`;
         featuresEL.appendChild(featureItem);
     }
-    stockEl.innerText = `In stock: ${parseInt(selectedLaptop.stock)}`;
-    imageEl.src = `${imageURL}${e.target.selectedIndex + 1}.png`;
+    currentLaptopId = selectedLaptop.id;
+    stockEl.innerText = stockArr[selectedLaptop.id];
+    imageEl.src = `${imageURL}${selectedLaptop.image}`;
 }
 
 //Function to clear list of features
@@ -121,4 +193,22 @@ function removeChildElements() {
     }
 }
 
+const payLaptop = () => {
+    if(currentLaptopPrice <= balance && stockArr[currentLaptopId] > 0) {
+        balance -= currentLaptopPrice;
+        stockArr[currentLaptopId] -= 1;
+        let formattedBalance = formatBalance(balance);
+        balanceLabel.innerText = formattedBalance;
+        stockEl.innerText = stockArr[currentLaptopId];
+        alert("Congratulations! Your are now an owner of a laptop!");
+    } else if (parseInt(currentLaptopPrice) >= balance) {
+        alert("You need to get some more money if you want this laptop.");
+    } else if (stockArr[currentLaptopId] <= 0) {
+        alert("Sorry! No more laptops left of this kind.");
+    } else {
+        alert("Error! Try again");
+    }
+}
+
 laptopEl.addEventListener("change", handleMenuChange);
+payButton.addEventListener('click', payLaptop)
